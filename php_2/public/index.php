dc67e2cefa66f406b1b4240098f740d1f2e8f688
julia-oop <kuznetsova1yulia@yandex.ru>
2021-10-09 20:47:03 +0800

    Update index.php

--- php_2/public/index.php
+++ php_2/public/index.php
@@ -1,38 +1,32 @@
 <?php
 
-// подключаем пакеты которые установили через composer
 require_once '../vendor/autoload.php';
-
-// создаем загрузчик шаблонов, и указываем папку с шаблонами
-// \Twig\Loader\FilesystemLoader -- это типа как в C# писать Twig.Loader.FilesystemLoader, 
-// только слеш вместо точек
 $loader = new \Twig\Loader\FilesystemLoader('../views');
 
-// создаем собственно экземпляр Twig с помощью которого будет рендерить
 $twig = new \Twig\Environment($loader);
 
 $url = $_SERVER["REQUEST_URI"];
 
 $title = "";
 $template = "";
-$context = []; 
 
-if ($url == "/") {
-    $title = "Главная";
-    $template = "main.twig";
-} elseif (preg_match("#/mermaid#", $url)) {
-    $title = "Русалка";
-    $template = "base_image.twig";
-    
-    $context['image'] = "/images/mermaid.jpg"; 
-} elseif (preg_match("#/uranus#", $url)) {
-    $title = "Уран";
-    $template = "base_image.twig";
-    
-    $context['image'] = "/images/uranus.png"; 
-}
-
+$context = [];
+$menu = [ 
+    [
+        "title" => "Главная",
+        "url" => "/",
+    ],
+    [
+        "title" => "Русалка",
+        "url" => "/mermaid",
+    ],
+    [
+        "title" => "Уран",
+        "url" => "/uranus",
+    ]
+];
 
 $context['title'] = $title;
+$context['menu'] = $menu; 
 
 echo $twig->render($template, $context);
