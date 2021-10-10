7a34d0a5df3b3d94dbec65eb86a2c0a70924e399
julia-oop <kuznetsova1yulia@yandex.ru>
2021-10-10 13:49:50 +0800

    arrr

--- php_2/public/index.php
+++ php_2/public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once '../vendor/autoload.php';
+
 $loader = new \Twig\Loader\FilesystemLoader('../views');
 
 $twig = new \Twig\Environment($loader);
@@ -10,23 +11,24 @@ $url = $_SERVER["REQUEST_URI"];
 $title = "";
 $template = "";
 
-$context = [];
-$menu = [ 
-    [
-        "title" => "Главная",
-        "url" => "/",
-    ],
-    [
-        "title" => "Русалка",
-        "url" => "/mermaid",
-    ],
-    [
-        "title" => "Уран",
-        "url" => "/uranus",
-    ]
-];
+$context = []; 
+
+if ($url == "/") {
+    $title = "Главная";
+    $template = "main.twig";
+} elseif (preg_match("#/mermaid#", $url)) {
+    $title = "Русалка";
+    $template = "__object.twig";
+    $context['image'] = "/images/mermaid.jpg"; 
+
+} elseif (preg_match("#/uranus#", $url)) {
+    $title = "Уран";
+    $template = "__object.twig";
+    $context['image'] = "/images/uranus.png"; 
+
+}
 
 $context['title'] = $title;
-$context['menu'] = $menu; 
 
 echo $twig->render($template, $context);
+?>
