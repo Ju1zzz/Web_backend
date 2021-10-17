1466e64c137c3c547bbc82ee1a5b445ff53af8ea
julia-oop <kuznetsova1yulia@yandex.ru>
2021-10-17 16:05:16 +0800

    done

--- php_3/controllers/KingImageController.php
+++ php_3/controllers/KingImageController.php
@@ -8,7 +8,6 @@ class KingImageController extends KingController {
     {
         $context = parent::getContext(); 
             $context['image'] = "/images/king.jpg";
-        $context['text'] = file_get_contents("C:/WEB-backend/php_3/views/king-about.php");
         $context['type'] = "image";
         return $context;
     }
--- php_3/controllers/MainController.php
+++ php_3/controllers/MainController.php
@@ -4,4 +4,33 @@ require_once "TwigBaseController.php";
 class MainController extends TwigBaseController {
     public $template = "main.twig";
     public $title = "Главная";
+
+    public function getContext() : array
+    {
+       
+        $context = parent::getContext(); 
+        $menu = [ 
+            [
+                "title" => "Ученик чудовища",
+                "url-main" => "/student",
+                "url-image" => "/student/image",
+                "url-info" => "/student/info"
+            ],
+            [
+                "title" => "Король Стейтен-Айленда",
+                "url-main" => "/king",
+                "url-image" => "/king/image",
+                "url-info" => "/king/info"
+            ],
+            [
+                "title" => "Игра в кальмара",
+                "url-main" => "/game",
+                "url-image" => "/game/image",
+                "url-info" => "/game/info"
+            ]
+        ];
+        $context['menu'] = $menu;
+        return $context;
+    }
+    
 }
